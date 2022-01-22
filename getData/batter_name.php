f94abcca77c165d195f8037d4513a0eb4f44a33c
MiMi1906 <mimi1906.rpm@gmail.com>
2022-01-22 17:35:28 +0900

    bug

--- getData/batter_name.php
+++ getData/batter_name.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('display_errors',  1);
+ini_set('display_errors', 1);
 require($_SERVER['DOCUMENT_ROOT'] . '/func.php');
 
 header("Content-Type: application/json; charset=UTF-8"); //ヘッダー情報の明記。必須。
